7f54374f59d758fd0336d5274ff2271ffd7c9933
Michael Schuett <michaeljs1990@gmail.com>
2019-01-30 21:42:06 -0500

    Add all flags for the modify command
    This ports all the flags over for the modify command. One strang thing
    is setting tag is done with t and tags instead of t and tag like is used
    on the query sub command. I'm going to leave it though to make moving to
    the new tooling easy.

--- modify.go
+++ modify.go
@@ -4,13 +4,66 @@ import (
 	cli "github.com/urfave/cli"
 )
 
+// Allowed values (uppercase or lowercase is accepted):
+//   Status:State (-S,--set-status):
+//     See `collins state --list`
+//   Log levels (-L,--level):
+//     ERROR, DEBUG, EMERGENCY, ALERT, CRITICAL, WARNING, NOTICE, INFORMATIONAL, NOTE
+
 func modifySubcommand() cli.Command {
 	return cli.Command{
 		Name:    "modify",
 		Aliases: []string{"set"},
 		Usage:   "Add and remove attributes, change statuses, and log to assets",
-		Action: func(c *cli.Context) error {
-			return nil
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:     "a, set-attribute",
+				Usage:    "Set attribute=value. : between key and value. attribute will be uppercased",
+				Category: "Modify options",
+			},
+			cli.StringFlag{
+				Name:     "d, delete-attribute",
+				Usage:    "Delete attribute",
+				Category: "Modify options",
+			},
+			cli.StringFlag{
+				Name:     "S, set-state",
+				Usage:    "Set attribute=value. : between key and value. attribute will be uppercased",
+				Category: "Modify options",
+			},
+			cli.StringFlag{
+				Name:     "S, set-state",
+				Usage:    "Set status (and optionally state) to status:state. Requires --reason",
+				Category: "Modify options",
+			},
+			cli.StringFlag{
+				Name:     "r, reason",
+				Usage:    "Reason for changing status/state",
+				Category: "Modify options",
+			},
+			cli.StringFlag{
+				Name:     "l, log",
+				Usage:    "Create a log entry",
+				Category: "Modify options",
+			},
+			cli.StringFlag{
+				Name:     "L, level",
+				Usage:    "Set log level. Default level is NOTE.",
+				Value:    "NOTE",
+				Category: "Modify options",
+			},
+			cli.StringFlag{
+				Name:     "t, tags",
+				Usage:    "Tags to work on, comma separated",
+				Category: "Modify options",
+			},
 		},
+		Action: modifyRunCommand,
 	}
+}
+
+func modifyRunCommand(c *cli.Context) error {
+	// client := getCollinsClient(c)
+	// assetService := client.AssetServices
+	return nil
 }
