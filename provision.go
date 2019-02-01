5c9a85523c29ab8f6de708389727d1c3d8d36bea
Michael Schuett <michaeljs1990@gmail.com>
2019-02-01 00:30:11 -0500

    Basic provision command

--- provision.go
+++ provision.go
@@ -1,15 +1,125 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"io"
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
 	cli "github.com/urfave/cli"
+	collins "gopkg.in/tumblr/go-collins.v0/collins"
 )
 
 func provisionSubcommand() cli.Command {
 	return cli.Command{
 		Name:  "provision",
 		Usage: "Provision assets",
-		Action: func(c *cli.Context) error {
-			return nil
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:     "n, nodeclass",
+				Usage:    "Nodeclass to provision as. (Required)",
+				Category: "Provision options",
+			},
+			cli.StringFlag{
+				Name:     "p, pool",
+				Usage:    "Provision with pool POOL",
+				Category: "Provision options",
+			},
+			cli.StringFlag{
+				Name:     "r, role",
+				Usage:    "Provision with primary role ROLE",
+				Category: "Provision options",
+			},
+			cli.StringFlag{
+				Name:     "R, secondary-role",
+				Usage:    "Provision with secondary role ROLE",
+				Category: "Provision options",
+			},
+			cli.StringFlag{
+				Name:     "s, suffix",
+				Usage:    "Provision with suffix SUFFIX",
+				Category: "Provision options",
+			},
+			cli.StringFlag{
+				Name:     "b, build-contact",
+				Usage:    "Build contact",
+				Value:    os.Getenv("USER"),
+				Category: "Provision options",
+			},
+			cli.StringFlag{
+				Name:     "t, tags",
+				Usage:    "Tags to work on, comma separated",
+				Category: "Provision options",
+			},
 		},
+		Action: provisionRunCommand,
 	}
+}
+
+// This is a little funny with the go collins api it taks an optional opts param
+// but it overrides half of this struct so we just fill the half it doesn't
+func provisionMakeOpts(ctx *cli.Context, tag string) collins.ProvisionOpts {
+
+	return collins.ProvisionOpts{
+		Suffix:        ctx.String("suffix"),
+		PrimaryRole:   ctx.String("role"),
+		SecondaryRole: ctx.String("secondary-role"),
+		Pool:          ctx.String("pool"),
+	}
+
+}
+
+func provisionByTag(ctx *cli.Context, col *collins.Client, tag string) {
+	if !ctx.IsSet("nodeclass") {
+		log.Fatal("You need to specify at least a nodeclass when provisioning")
+	}
+
+	opts := provisionMakeOpts(ctx, tag)
+	profile := ctx.String("nodeclass")
+	contact := ctx.String("build-contact")
+	_, err := col.Management.Provision(tag, profile, contact, opts)
+	msg := tag + " provisioning with nodeclass:" + profile + " by " + contact + "... "
+	if err != nil {
+		gotError = true
+		log.Error(msg + "ERROR")
+	} else {
+		log.Print(msg + "SUCCESS")
+	}
+}
+
+func provisionRunCommand(c *cli.Context) error {
+	client := getCollinsClient(c)
+
+	if c.IsSet("tags") {
+		tags := strings.Split(c.String("tags"), ",")
+		for _, tag := range tags {
+			provisionByTag(c, client, tag)
+		}
+	} else {
+		// No tag was passed in try to read from stdin
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			line, err := reader.ReadString('\n')
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				log.Fatal(err)
+			}
+
+			// If a newline was all that was recieved from stdin
+			// ignore it and keep going.
+			tag := strings.Fields(line)
+			if len(tag) >= 1 {
+				provisionByTag(c, client, tag[0])
+			}
+		}
+	}
+
+	if gotError {
+		return errors.New("Some commands failed to run to success")
+	}
+
+	return nil
 }
