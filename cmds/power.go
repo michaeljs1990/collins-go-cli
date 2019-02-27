4da9a52990fd93eebd5b706bc2e4b6e205154a91
Michael Schuett <michaeljs1990@gmail.com>
2019-02-26 23:36:49 -0500

    Add concurrent power command
    Make the power command more resilient against bad BMCs. Instead of
    waiting for timeouts one by one we can now wait for multiple together!

--- cmds/power.go
+++ cmds/power.go
@@ -7,7 +7,9 @@ import (
 	"io"
 	"os"
 	"strings"
+	"sync"
 
+	color "github.com/logrusorgru/aurora"
 	cli "github.com/urfave/cli"
 	collins "gopkg.in/tumblr/go-collins.v0/collins"
 )
@@ -32,6 +34,12 @@ func PowerSubcommand() cli.Command {
 				Usage:    "Tags to work on, comma separated",
 				Category: "Power options",
 			},
+			cli.IntFlag{
+				Name:     "n, number",
+				Usage:    "number of operations to run concurrently",
+				Category: "Power options",
+				Value:    10,
+			},
 		},
 		Action: powerRunCommand,
 	}
@@ -41,38 +49,38 @@ func PowerSubcommand() cli.Command {
 // status are passed in we just ignore the status request and run the
 // specified power command. Maybe remove this and allow you to specify
 // both in a later release.
-func powerActionByTag(ctx *cli.Context, col *collins.Client, tag string) {
+func powerActionByTag(wg *sync.WaitGroup, ctx *cli.Context, col *collins.Client, tag string) {
+	wg.Add(1)
+	defer wg.Done()
 
 	if ctx.IsSet("power") {
 		var err error
+		msg := tag + " performing " + ctx.String("power") + " ... "
 		switch ctx.String("power") {
 		case "reboot", "rebootSoft":
-			fmt.Print(tag + " performing " + ctx.String("power") + " ... ")
 			_, err = col.Management.SoftReboot(tag)
 		case "reboothard":
-			fmt.Print(tag + " performing " + ctx.String("power") + " ... ")
 			_, err = col.Management.HardReboot(tag)
 		case "poweron", "on":
-			fmt.Print(tag + " performing " + ctx.String("power") + " ... ")
 			_, err = col.Management.PowerOn(tag)
 		case "poweroff", "off":
-			fmt.Print(tag + " performing " + ctx.String("power") + " ... ")
 			_, err = col.Management.SoftPowerOff(tag)
 		case "identify":
-			fmt.Print(tag + " performing " + ctx.String("power") + " ... ")
 			_, err = col.Management.Identify(tag)
 		case "verify":
-			fmt.Print(tag + " performing " + ctx.String("power") + " ... ")
 			_, err = col.Management.Verify(tag)
 		default:
 			logAndDie("Unknown power action rebootx, expecting one of reboot,rebootsoft,reboothard,on,off,poweron,poweroff,identify,verify")
 		}
 
+		// We dont' use printSuccess or printError here since these are happening in a go routine
+		// and although it's unlikely it's possibly that these don't print in the right order if it's
+		// not all printed to screen in one fmt.Print function.
 		if err != nil {
 			gotError = true
-			printError(err.Error())
+			fmt.Print(msg, color.Red("ERROR"))
 		} else {
-			printSuccess()
+			fmt.Print(msg, color.Green("SUCCESS"))
 		}
 
 		return
@@ -80,7 +88,6 @@ func powerActionByTag(ctx *cli.Context, col *collins.Client, tag string) {
 
 	if ctx.IsSet("status") {
 		msg := tag + " checking power status ... "
-		fmt.Print(msg)
 
 		stat, _, err := col.Management.PowerStatus(tag)
 
@@ -92,7 +99,7 @@ func powerActionByTag(ctx *cli.Context, col *collins.Client, tag string) {
 			gotError = true
 		}
 
-		fmt.Print("(" + stat + ")\n")
+		fmt.Print(msg+"(", color.Magenta(stat), ")\n")
 
 		return
 	}
@@ -102,10 +109,20 @@ func powerActionByTag(ctx *cli.Context, col *collins.Client, tag string) {
 func powerRunCommand(c *cli.Context) error {
 	client := getCollinsClient(c)
 
+	if c.Int("number") <= 0 {
+		logAndDie("--number must be greater than 0")
+	}
+
+	var wg sync.WaitGroup
+	powerActionsRun := 0
 	if c.IsSet("tags") {
 		tags := strings.Split(c.String("tags"), ",")
 		for _, tag := range tags {
-			powerActionByTag(c, client, tag)
+			powerActionsRun++
+			go powerActionByTag(&wg, c, client, tag)
+			if 0 == powerActionsRun%c.Int("number") {
+				wg.Wait()
+			}
 		}
 	} else {
 		// No tag was passed in try to read from stdin
@@ -122,10 +139,21 @@ func powerRunCommand(c *cli.Context) error {
 			// ignore it and keep going.
 			tag := strings.Fields(line)
 			if len(tag) >= 1 {
-				powerActionByTag(c, client, tag[0])
+				powerActionsRun++
+				go powerActionByTag(&wg, c, client, tag[0])
+			}
+
+			// In the case that the actions run are a mod of number
+			// we take a break and wait for them to finish up before
+			// issuing more commands.
+			if 0 == powerActionsRun%c.Int("number") {
+				wg.Wait()
 			}
 		}
 	}
+
+	// Ensure nothing is outstanding
+	wg.Wait()
 
 	if gotError {
 		return errors.New("Some commands failed to run to success")
