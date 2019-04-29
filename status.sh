1691c4bcc5a2e8fb88f7f9ca54f9107706681ac4
Michael Schuett <michaeljs1990@gmail.com>
2019-04-28 20:42:58 -0400

    Cleanup typo

--- status.sh
+++ status.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Produce environment variable that we can use inside out build files.
+# Produce environment variable that we can use inside our build files.
 # Mostly used to pass in environment variables to golang ldflags.
-# Vars with STABLE_* format cause builds to retrigger.
+# Vars with STABLE_* format cause builds to retrigger when evaluated
+# by bazel.
 echo "STABLE_GIT_COMMIT $(git rev-parse HEAD)"
 echo "STABLE_GIT_TAG $(git describe)"
